91712d01e72f21c024b0374df8868667757d74c4
AlariCode <antonlarichev@gmail.com>
2024-07-18 16:23:52 +0300

    Упражнение - Ускорение работы

--- main.go
+++ main.go
@@ -2,16 +2,26 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"time"
 )
 
+// 10 конкурентных запросов на GET по адресу google.com
+// Вывести в консоль 10 StatusCode
+
 func main() {
-	go printHi()
-	go fmt.Println("Привет из main 2")
-	go fmt.Println("Привет из main")
-	time.Sleep(time.Second)
+	t := time.Now()
+	for i := 0; i < 10; i++ {
+		go getHttpCode()
+	}
+	time.Sleep(time.Millisecond * 1100)
+	fmt.Println(time.Since(t))
 }
 
-func printHi() {
-	fmt.Println("Привет из gr")
+func getHttpCode() {
+	resp, err := http.Get("https://google.com")
+	if err != nil {
+		fmt.Printf("Ошибка %s", err.Error())
+	}
+	fmt.Printf("Код: %d\n", resp.StatusCode)
 }
