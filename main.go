7a4ec45f402db48dff40b8affec5a4cd61c3cb9f
AlariCode <antonlarichev@gmail.com>
2024-07-25 12:31:40 +0300

    Создание канала

--- main.go
+++ main.go
@@ -3,31 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"sync"
-	"time"
 )
 
-// 10 конкурентных запросов на GET по адресу google.com
-// Вывести в консоль 10 StatusCode
-
 func main() {
-	t := time.Now()
-	var wg sync.WaitGroup
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go func() {
-			getHttpCode()
-			wg.Done()
-		}()
-	}
-	wg.Wait()
-	fmt.Println(time.Since(t))
+	code := make(chan int)
+	go getHttpCode(code)
+	<-code
 }
 
-func getHttpCode() {
+func getHttpCode(codeCh chan int) {
 	resp, err := http.Get("https://google.com")
 	if err != nil {
 		fmt.Printf("Ошибка %s", err.Error())
 	}
 	fmt.Printf("Код: %d\n", resp.StatusCode)
+	codeCh <- resp.StatusCode
 }
