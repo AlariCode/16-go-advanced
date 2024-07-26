33189fdb755a034ce6920092cd326e5b549b11f2
AlariCode <antonlarichev@gmail.com>
2024-07-26 09:41:45 +0300

    Закрытие канала

--- main.go
+++ main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 )
 
 func main() {
 	code := make(chan int)
+	var wg sync.WaitGroup
 	for i := 0; i < 10; i++ {
-		go getHttpCode(code)
+		wg.Add(1)
+		go func() {
+			getHttpCode(code)
+			wg.Done()
+		}()
 	}
+	go func() {
+		wg.Wait()
+		close(code)
+	}()
 	for res := range code {
 		fmt.Printf("Код: %d\n", res)
 	}
