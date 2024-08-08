22b1a034bb854a3354ac6247cfb2c47bb6615aef
Alaricode <antonlarichev@gmail.com>
2024-08-08 15:54:15 +0300

    Простейший сервер

--- main.go
+++ main.go
@@ -1,41 +1,16 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"net/http"
-	"os"
-	"strings"
 )
 
-func ping(url string, respCh chan int, errCh chan error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		errCh <- err
-		return
-	}
-	respCh <- resp.StatusCode
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Hello")
 }
 
 func main() {
-	path := flag.String("file", "url.txt", "path to URL file")
-	flag.Parse()
-	file, err := os.ReadFile(*path)
-	if err != nil {
-		panic(err.Error())
-	}
-	urlSlice := strings.Split(string(file), "\n")
-	respCh := make(chan int)
-	errCh := make(chan error)
-	for _, url := range urlSlice {
-		go ping(url, respCh, errCh)
-	}
-	for range urlSlice {
-		select {
-		case err := <-errCh:
-			fmt.Println(err)
-		case res := <-respCh:
-			fmt.Println(res)
-		}
-	}
+	http.HandleFunc("/hello", hello)
+	fmt.Println("Server is listening on port 8081")
+	http.ListenAndServe(":8081", nil)
 }
