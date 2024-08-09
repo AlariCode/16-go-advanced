da7f4294a210d52579c6dfb95522a85c3ee54cec
Alaricode <antonlarichev@gmail.com>
2024-08-09 14:56:29 +0300

    Свой ServeMux

--- main.go
+++ main.go
@@ -10,7 +10,14 @@ func hello(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/hello", hello)
+	router := http.NewServeMux()
+	router.HandleFunc("/hello", hello)
+
+	server := http.Server{
+		Addr:    ":8081",
+		Handler: router,
+	}
+
 	fmt.Println("Server is listening on port 8081")
-	http.ListenAndServe(":8081", nil)
+	server.ListenAndServe()
 }
