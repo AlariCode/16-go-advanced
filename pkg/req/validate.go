c2c797b1cac8d5785e3e644bcf84cd85d36e6513
Alaricode <antonlarichev@gmail.com>
2024-09-04 10:25:59 +0300

    Вынос обработчика

---
+++ pkg/req/validate.go
@@ -0,0 +1,11 @@
+package req
+
+import (
+	"github.com/go-playground/validator/v10"
+)
+
+func IsValid[T any](payload T) error {
+	validate := validator.New()
+	err := validate.Struct(payload)
+	return err
+}
