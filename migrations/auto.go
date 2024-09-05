2499e99cd8dcf8cfe4a27c751e63bad70cab07d3
Alaricode <antonlarichev@gmail.com>
2024-09-05 12:45:05 +0300

    Авто миграции

---
+++ migrations/auto.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"go/adv-demo/internal/link"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(postgres.Open(os.Getenv("DSN")), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&link.Link{})
+}
