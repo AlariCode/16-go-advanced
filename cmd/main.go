0842dff684ec13f7b2652bbbed07952cc6d717eb
Alaricode <antonlarichev@gmail.com>
2024-09-13 16:05:36 +0300

    Stack middleware

--- cmd/main.go
+++ cmd/main.go
@@ -26,9 +26,15 @@ func main() {
 		LinkRepository: linkRepository,
 	})
 
+	// Middlewares
+	stack := middleware.Chain(
+		middleware.CORS,
+		middleware.Logging,
+	)
+
 	server := http.Server{
 		Addr:    ":8081",
-		Handler: middleware.CORS(middleware.Logging(router)),
+		Handler: stack(router),
 	}
 
 	fmt.Println("Server is listening on port 8081")
