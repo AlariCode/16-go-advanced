a461fb03191f21f428787ae9524d8191c34d2223
Alaricode <antonlarichev@gmail.com>
2024-09-18 16:54:27 +0300

    Bcrypt

--- internal/auth/service.go
+++ internal/auth/service.go
@@ -3,6 +3,8 @@ package auth
 import (
 	"errors"
 	"go/adv-demo/internal/user"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthService struct {
@@ -18,12 +20,16 @@ func (service *AuthService) Register(email, password, name string) (string, erro
 	if existedUser != nil {
 		return "", errors.New(ErrUserExists)
 	}
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
 	user := &user.User{
 		Email:    email,
-		Password: "",
+		Password: string(hashedPassword),
 		Name:     name,
 	}
-	_, err := service.UserRepository.Create(user)
+	_, err = service.UserRepository.Create(user)
 	if err != nil {
 		return "", err
 	}
