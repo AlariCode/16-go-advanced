2d755c0004d6049395b39678cd491331a481fae7
Alaricode <antonlarichev@gmail.com>
2024-09-20 12:27:30 +0300

    Запись в контекст

--- pkg/middleware/auth.go
+++ pkg/middleware/auth.go
@@ -1,20 +1,26 @@
 package middleware
 
 import (
-	"fmt"
+	"context"
 	"go/adv-demo/configs"
 	"go/adv-demo/pkg/jwt"
 	"net/http"
 	"strings"
 )
 
+type key string
+
+const (
+	ContextEmailKey key = "ContextEmailKey"
+)
+
 func IsAuthed(next http.Handler, config *configs.Config) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authedHeader := r.Header.Get("Authorization")
 		token := strings.TrimPrefix(authedHeader, "Bearer ")
-		isValid, data := jwt.NewJWT(config.Auth.Secret).Parse(token)
-		fmt.Println(isValid)
-		fmt.Println(data)
-		next.ServeHTTP(w, r)
+		_, data := jwt.NewJWT(config.Auth.Secret).Parse(token)
+		ctx := context.WithValue(r.Context(), ContextEmailKey, data.Email)
+		req := r.WithContext(ctx)
+		next.ServeHTTP(w, req)
 	})
 }
