f33e25183add541d0df5cfa57a68367f142638b3
Alaricode <antonlarichev@gmail.com>
2024-09-25 09:53:18 +0300

    Unauth

--- pkg/middleware/auth.go
+++ pkg/middleware/auth.go
@@ -14,11 +14,24 @@ const (
 	ContextEmailKey key = "ContextEmailKey"
 )
 
+func writeUnauthed(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusUnauthorized)
+	w.Write([]byte(http.StatusText(http.StatusUnauthorized)))
+}
+
 func IsAuthed(next http.Handler, config *configs.Config) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authedHeader := r.Header.Get("Authorization")
+		if !strings.HasPrefix(authedHeader, "Bearer ") {
+			writeUnauthed(w)
+			return
+		}
 		token := strings.TrimPrefix(authedHeader, "Bearer ")
-		_, data := jwt.NewJWT(config.Auth.Secret).Parse(token)
+		isValid, data := jwt.NewJWT(config.Auth.Secret).Parse(token)
+		if !isValid {
+			writeUnauthed(w)
+			return
+		}
 		ctx := context.WithValue(r.Context(), ContextEmailKey, data.Email)
 		req := r.WithContext(ctx)
 		next.ServeHTTP(w, req)
