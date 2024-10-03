739c52700ca287e862321dcd61e1a11b3b943b3d
Alaricode <antonlarichev@gmail.com>
2024-10-03 12:55:07 +0300

    E2e тест

---
+++ cmd/auth_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"go/adv-demo/internal/auth"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestLoginSuccess(t *testing.T) {
+	ts := httptest.NewServer(App())
+	defer ts.Close()
+
+	data, _ := json.Marshal(&auth.LoginRequest{
+		Email:    "a2@a.ru",
+		Password: "1",
+	})
+
+	res, err := http.Post(ts.URL+"/auth/login", "application/json", bytes.NewReader(data))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.StatusCode != 200 {
+		t.Fatalf("Expected %d got %d", 200, res.StatusCode)
+	}
+}
