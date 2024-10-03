c52b0ca6750e3e58583a5418bd9a023734709bd1
Alaricode <antonlarichev@gmail.com>
2024-10-03 13:05:51 +0300

    Упражнение - Отрицательный тест

--- cmd/auth_test.go
+++ cmd/auth_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"go/adv-demo/internal/auth"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -24,5 +25,35 @@ func TestLoginSuccess(t *testing.T) {
 	}
 	if res.StatusCode != 200 {
 		t.Fatalf("Expected %d got %d", 200, res.StatusCode)
+	}
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var resData auth.LoginResponse
+	err = json.Unmarshal(body, &resData)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resData.Token == "" {
+		t.Fatal("Token empty")
+	}
+}
+
+func TestLoginFail(t *testing.T) {
+	ts := httptest.NewServer(App())
+	defer ts.Close()
+
+	data, _ := json.Marshal(&auth.LoginRequest{
+		Email:    "a2@a.ru",
+		Password: "2",
+	})
+
+	res, err := http.Post(ts.URL+"/auth/login", "application/json", bytes.NewReader(data))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.StatusCode != 401 {
+		t.Fatalf("Expected %d got %d", 401, res.StatusCode)
 	}
 }
