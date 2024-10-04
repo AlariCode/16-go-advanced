093e8ded17d54e9882e18d7eb5968f86a34c01c1
Alaricode <antonlarichev@gmail.com>
2024-10-04 17:08:17 +0300

    Unit тесты

---
+++ pkg/jwt/jwt_test.go
@@ -0,0 +1,24 @@
+package jwt_test
+
+import (
+	"go/adv-demo/pkg/jwt"
+	"testing"
+)
+
+func TestJWTCreate(t *testing.T) {
+	const email = "a@a.ru"
+	jwtService := jwt.NewJWT("/2+XnmJGz1j3ehIVI/5P9kl+CghrE3DcS7rnT+qar5w=")
+	token, err := jwtService.Create(jwt.JWTData{
+		Email: email,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	isValid, data := jwtService.Parse(token)
+	if !isValid {
+		t.Fatal("Token is invalid")
+	}
+	if data.Email != email {
+		t.Fatalf("Email %s not equal %s", data.Email, email)
+	}
+}
