ace0f600db65834d86ffc9e201dd0688a8da5aa0
Alaricode <antonlarichev@gmail.com>
2024-10-04 16:58:26 +0300

    Очистка данных

--- cmd/auth_test.go
+++ cmd/auth_test.go
@@ -36,6 +36,12 @@ func initData(db *gorm.DB) {
 	})
 }
 
+func removeData(db *gorm.DB) {
+	db.Unscoped().
+		Where("email = ?", "a2@a.ru").
+		Delete(&user.User{})
+}
+
 func TestLoginSuccess(t *testing.T) {
 	// Prepare
 	db := initDb()
@@ -68,9 +74,12 @@ func TestLoginSuccess(t *testing.T) {
 	if resData.Token == "" {
 		t.Fatal("Token empty")
 	}
+	removeData(db)
 }
 
 func TestLoginFail(t *testing.T) {
+	db := initDb()
+	initData(db)
 	ts := httptest.NewServer(App())
 	defer ts.Close()
 
@@ -86,4 +95,5 @@ func TestLoginFail(t *testing.T) {
 	if res.StatusCode != 401 {
 		t.Fatalf("Expected %d got %d", 401, res.StatusCode)
 	}
+	removeData(db)
 }
