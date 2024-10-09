b5344158225d8c50bb094af1e3aae2154b2ab8ea
Alaricode <antonlarichev@gmail.com>
2024-10-09 11:35:48 +0300

    Mock запросов

--- internal/auth/handler_test.go
+++ internal/auth/handler_test.go
@@ -42,7 +42,10 @@ func bootstrap() (*auth.AuthHandler, sqlmock.Sqlmock, error) {
 }
 
 func TestLoginSuccess(t *testing.T) {
-	handler, _, err := bootstrap()
+	handler, mock, err := bootstrap()
+	rows := sqlmock.NewRows([]string{"email", "password"}).
+		AddRow("a2@a.ru", "$2a$10$fOFzfotZx.uhK2BkJTy4AuVb6ejteFYEUkREKD/nBR6fZx4afcmYS")
+	mock.ExpectQuery("SELECT").WillReturnRows(rows)
 	if err != nil {
 		t.Fatal(err)
 		return
